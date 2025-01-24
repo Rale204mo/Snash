6b15d4faf24620d1f336c9d450b2c185d36cbe43
Leakay_fx <motsamairalejoe@gmail.com>
2025-01-24 15:44:18 +0200

    message.php

---
+++ snash dah fx/message.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    // You can save the data to a database or send it via email
+    $to = "your-email@example.com";
+    $subject = "New Contact Message from $name";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message sending failed.";
+    }
+}
+?>
